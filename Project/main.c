db3b51409f980c64dc5be81d38d92cdaa4584261
NgThienn <109409996+NgThienn@users.noreply.github.com>
2024-04-03 10:53:11 +0700

    update

--- Project/main.c
+++ Project/main.c
@@ -1,16 +1,9 @@
-/*
- * Project.c
- *
- * Created: 4/3/2024 10:35:17 AM
- * Author : os
- */ 
-
 #include <avr/io.h>
-
+#include <LCD.h>
+#include <Sensor.h>
 
 int main(void)
 {
-    /* Replace with your application code */
     while (1) 
     {
     }
