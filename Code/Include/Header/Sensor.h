d54b281cbc0994ef0f41391cbbe3bb43ab315da6
NgThienn <109409996+NgThienn@users.noreply.github.com>
2024-04-11 00:47:54 +0700

    update

--- Code/Include/Header/Sensor.h
+++ Code/Include/Header/Sensor.h
@@ -1,5 +1,4 @@
 #ifndef SENSOR_H_
 #define SENSOR_H_
-void ADC_Init();
-int ADC_Read(char channel);
+
 #endif /* SENSOR_H_ */
--- Code/main.c
+++ Code/main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <LCD.h>
 #include <Sensor.h>
-
+#define F_CPU 12000000ul
 int main(void)
 {
     while (1) 
